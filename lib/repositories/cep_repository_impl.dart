46ba74766f42946e4f8596959a3b549feee9ce63
Lucascluz <80330722+Lucascluz@users.noreply.github.com>
2023-02-06 22:30:16 -0300

    Little updates

--- lib/repositories/cep_repository_impl.dart
+++ lib/repositories/cep_repository_impl.dart
@@ -1,9 +1,18 @@
+import 'dart:math';
+
+import 'package:cep_app/models/endereco_model.dart';
 import 'package:dio/dio.dart';
 import 'cep_repository.dart';
 
 class CepRepositoryImpl implements CepRepository {
-  Future getCep(String cep) async {
-    final result = await Dio().get('https://viacep.com.br/ws/$cep/json');
-    result.data;
+  @override
+  Future<EnderecoModel> getCep(String cep) async {
+    try {
+      final result = await Dio().get('https://viacep.com.br/ws/$cep/json');
+      return EnderecoModel.fromMap(result.data);
+    } on DioError catch (e) {
+      log('Erro ao buscar CEP', error: e);
+      throw Exception('Erro ao buscar CEP');
+    }
   }
 }
